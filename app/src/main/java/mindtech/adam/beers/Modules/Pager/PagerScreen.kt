8dfc57bcd773eacf01140834b646069c1b2403aa
Adam Cseke <cseke.adam18@gmail.com>
2024-02-01 15:38:32 +0100

    feat: List after 10th beer

--- app/src/main/java/mindtech/adam/beers/Modules/Pager/PagerScreen.kt
+++ app/src/main/java/mindtech/adam/beers/Modules/Pager/PagerScreen.kt
@@ -1,45 +1,32 @@
 package mindtech.adam.beers.Modules.Pager
 
-import android.content.Context
-import android.widget.Toast
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import mindtech.adam.beers.Modules.MainViewModel
 import mindtech.adam.beers.Data.Models.Beer
 import coil.compose.rememberAsyncImagePainter
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import mindtech.adam.beers.Modules.Pager.Views.BeerItem
-import mindtech.adam.beers.ui.theme.DislikeRed
-import mindtech.adam.beers.ui.theme.LikeGreen
+import mindtech.adam.beers.ui.theme.BeersTheme
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -68,9 +55,52 @@ fun PagerScreen(viewModel: MainViewModel = hiltViewModel()) {
 
 @Composable
 fun FavoriteBeersList(favoritBeers: List<Beer>) {
-    Column {
-        favoritBeers.forEach { beer ->
-            Text(beer.name)
+    LazyColumn {
+        items(favoritBeers) {beer ->
+            Row {
+                Image(
+                    painter = rememberAsyncImagePainter(beer.imageURL),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(128.dp)
+                        .padding(
+                            top = 22.dp,
+                        )
+                )
+
+                Column {
+                    Text(modifier = Modifier
+                        .padding(
+                            top = 12.dp,
+                        ),
+                        text = beer.name)
+
+                    Text(modifier = Modifier
+                        .padding(
+                            bottom = 12.dp
+                        ),
+                        text = beer.tagline)
+                }
+            }
+            Divider(modifier = Modifier
+                .padding(horizontal = 12.dp,
+                    vertical = 12.dp),
+                )
         }
+    }
+}
+
+fun getDummyBeers(): List<Beer> {
+    return listOf(
+        Beer(id = 1, name = "Beer 1", tagline = "Tagline 1", imageURL = "https://images.punkapi.com/v2/7.png"),
+        Beer(id = 2, name = "Beer 2", tagline = "Tagline 2", imageURL = "https://images.punkapi.com/v2/2.png"),
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BeerListPreview() {
+    BeersTheme {
+        FavoriteBeersList(favoritBeers = getDummyBeers())
     }
 }
